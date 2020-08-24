167b933ed44fe697d3fc9bf4dd9b8538f49479c2
liushaoquan <liushaoquan@aiyunxiao.com>
2020-08-25 07:56:41 +0800

    dev

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
+app.use('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'dist/index.html'));
+})
+
 app.listen(3000, () => {
     console.log('listen 3000')
 })
+
