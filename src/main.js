b1173d8b8c6f02c5faa619a02c1e2ac416dd4a45
liushaoquan <liushaoquan@aiyunxiao.com>
2020-08-25 08:00:47 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -11,9 +11,11 @@ new Vue({
 
 var _hmt = _hmt || [];
 window._hmt = _hmt; // 必须把_hmt挂载到window下，否则找不到
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?"+ 百度站点id;
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b29a5342f4b6dba9fdfd4712f7a05453";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
 })();
+
+
