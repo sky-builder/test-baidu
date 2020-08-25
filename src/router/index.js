0065bd23a392b112a30bca2a9062f8a7beb275f3
liushaoquan <liushaoquan@aiyunxiao.com>
2020-08-25 08:40:43 +0800

    dev

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Test from '../views/Test.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: Test
   }
 ]
 
@@ -26,9 +32,9 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-    if (to.path) window._hmt.push(['_trackPageview', to.fullPath])
-    next();
-})
+// router.beforeEach((to, from, next) => {
+//     if (to.path) window._hmt.push(['_trackPageview', to.fullPath])
+//     next();
+// })
 
 export default router
