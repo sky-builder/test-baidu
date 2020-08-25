5e7b8140f892246ba9288cf6751043f72896b98f
liushaoquan <liushaoquan@aiyunxiao.com>
2020-08-25 08:26:28 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -9,13 +9,5 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-var _hmt = _hmt || [];
-window._hmt = _hmt; // 必须把_hmt挂载到window下，否则找不到
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?b29a5342f4b6dba9fdfd4712f7a05453";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
 
 
